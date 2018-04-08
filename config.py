232730446d021b7e69f07a8c5e4f7c81da940a26
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-09 01:53:03 +0200

    Working on config

---
+++ config.py
@@ -0,0 +1,15 @@
+language = "IT"
+
+NAO_IP = "127.0.0.1"
+NAO_PORT = 9559
+
+tetheringSSID = "Nao-WiFi"
+tetheringPassword = "nao12345"
+wifiCountry = "IT"
+
+SaySSID = True
+SpellSSID = False
+SayPassowrd = False
+SpellPassword = False
+
+logMode = "Both"
