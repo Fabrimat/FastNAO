1769b61353058eefb34155c4f9645dbbeb9b68c5
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-09 01:53:27 +0200

    Working on config

---
+++ EN-lang.py
@@ -0,0 +1,40 @@
+ChorDiscSucc = "Choregraphe connection removed successfully."
+ChorDiscFail = "Choregraphe connection not found."
+ChorDiscError = "Error removing Choregraphe connection."
+
+Welcome1 = "You entered the advanced menu, hope you know what you are doing!"
+Welcome2 = "Use my head sensors to move between the options."
+
+OffsetChange = "Offset: %s"
+OffsetSet = "Offset set to %s!"
+OffsetChangeFail = "Cannot do it"
+
+Unknown error = "Unknown error."
+
+MainMenuChange = "%s selected!"
+
+NothingSelected = "Nothing selected! Quitting."
+
+WiFiError = "Error, ALConnectionManager is no longer avaiable"
+WifiActivated = "Wifi Tethering activated."
+WifiAlreadyActive = "Wifi Tethering is already active."
+WifiDeactivated = "Wifi Tethering deactivated."
+WifiAlreadyInactive = "Wifi Tethering is already inactive."
+
+VirtualRobotName = "virtual robot"
+
+Name = "My name is %s \pau=300"
+Battery = "My battery is at %d percent \pau=300"
+Autonomous = "My autonomous life status is: %s"
+WifiActive = "WiFi Tethering is active"
+UpperLetter = "Upper %s \pau=100 "
+LowerLetter = "Lower %s \pau=100 "
+WifiSSID = "WiFi Tethering SSID is %s"
+WifiPassword = "WiFi Tethering password is %s"
+WifiInactive = "WiFi Tethering is not active"
+InternetOn = "I am connected to the Internet"
+InternetOff = "I am not connected to the Internet"
+
+Surrdender = "I surrender."
+
+FastReboot = "Rebooting!"
