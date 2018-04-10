8d3caa1628c707f14f28bbacfc50842945f9e6d0
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-10 23:09:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+#  ______        _   _   _          ____  
+# |  ____|      | | | \ | |   /\   / __ \ 
+# | |__ __ _ ___| |_|  \| |  /  \ | |  | |
+# |  __/ _` / __| __| . ` | / /\ \| |  | |
+# | | | (_| \__ \ |_| |\  |/ ____ \ |__| |
+# |_|  \__,_|___/\__|_| \_/_/    \_\____/   
+
 # The language file you will be using
 # IT stands for IT-lang.py
 Language = "IT"
@@ -7,8 +14,11 @@ Nao_IP = "127.0.0.1"
 # Naoqi port
 Nao_Port = 9559
 
+# Nao WiFi network's SSID
 Tethering_SSID = "Nao-WiFi"
+# Nao WiFi network's password
 Tethering_Password = "nao12345"
+# WiFi country, you should use your own country to avoid fines
 Wifi_Country = "IT"
 
 # Say SSID during Status
@@ -20,7 +30,13 @@ Say_Passowrd = False
 # Spell Password or not
 Spell_Password = False
 
-# Whether to log to file or to naoqi
-# Options: file, naoqi, both
-# Case insensitive
-Log_Mode = "Both"
+# The name of the log file
+Log_File_Name = "FastNAO.log"
+# The format of every line of log
+Log_Format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+# Say the message when pressing three times on the chest button
+Intro = True
+
+# Do not change!
+Config_Version = 0.2
