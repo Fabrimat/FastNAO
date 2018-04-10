d2613f55374aca989748dbd805cb0c1652d0565b
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-10 23:10:19 +0200

    Update EN-lang.py

--- EN-lang.py
+++ EN-lang.py
@@ -1,3 +1,12 @@
+#  ______        _   _   _          ____  
+# |  ____|      | | | \ | |   /\   / __ \ 
+# | |__ __ _ ___| |_|  \| |  /  \ | |  | |
+# |  __/ _` / __| __| . ` | / /\ \| |  | |
+# | | | (_| \__ \ |_| |\  |/ ____ \ |__| |
+# |_|  \__,_|___/\__|_| \_/_/    \_\____/  
+
+LanguageName = "English"
+
 ChorDiscSucc = "Choregraphe connection removed successfully."
 ChorDiscFail = "Choregraphe connection not found."
 ChorDiscError = "Error removing Choregraphe connection."
@@ -9,9 +18,19 @@ OffsetChange = "Offset: %s"
 OffsetSet = "Offset set to %s!"
 OffsetChangeFail = "Cannot do it"
 
-Unknown error = "Unknown error."
+UnknownError = "Unknown error."
 
 MainMenuChange = "%s selected!"
+
+Disconnect = "Disconnect Choregraphe"
+WiFiOn = "Activate WiFi"
+WiFiOff = "Deactivate WiFi"
+AutoLifeToggle = "Autonomous Life Toggle"
+ChangeOffsetFloor = "Change Offset from floor"
+ChangeVolume = "Change volume"
+Status = "Status"
+FastReboot = "Fast Reboot"
+Close = "Close the menu"
 
 NothingSelected = "Nothing selected! Quitting."
 
@@ -21,14 +40,18 @@ WifiAlreadyActive = "Wifi Tethering is already active."
 WifiDeactivated = "Wifi Tethering deactivated."
 WifiAlreadyInactive = "Wifi Tethering is already inactive."
 
-VirtualRobotName = "virtual robot"
+VirtualRobotName = "Virtual Robot"
 
 Name = "My name is %s \pau=300"
 Battery = "My battery is at %d percent \pau=300"
 Autonomous = "My autonomous life status is: %s"
+AutoSolitary = "Solitary"
+AutoInteractive = "Interactive"
+AutoDisabled = "Disabled"
+AutoSafeGuard = "SafeGuard"
 WifiActive = "WiFi Tethering is active"
-UpperLetter = "Upper %s \pau=100 "
-LowerLetter = "Lower %s \pau=100 "
+UpperLetter = "Upper %s \pau=100"
+LowerLetter = "Lower %s \pau=100"
 WifiSSID = "WiFi Tethering SSID is %s"
 WifiPassword = "WiFi Tethering password is %s"
 WifiInactive = "WiFi Tethering is not active"
@@ -37,4 +60,7 @@ InternetOff = "I am not connected to the Internet"
 
 Surrdender = "I surrender."
 
-FastReboot = "Rebooting!"
+Reboot = "Rebooting!"
+
+# Do not change!
+Config_Version = 0.2
