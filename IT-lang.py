e7d51d11dd302278fd305c077e74ce330093cfd2
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-10 23:10:36 +0200

    Create IT-lang.py

---
+++ IT-lang.py
@@ -0,0 +1,66 @@
+#  ______        _   _   _          ____  
+# |  ____|      | | | \ | |   /\   / __ \ 
+# | |__ __ _ ___| |_|  \| |  /  \ | |  | |
+# |  __/ _` / __| __| . ` | / /\ \| |  | |
+# | | | (_| \__ \ |_| |\  |/ ____ \ |__| |
+# |_|  \__,_|___/\__|_| \_/_/    \_\____/  
+
+LanguageName = "Italiano"
+
+ChorDiscSucc = "Connessione a Choregraphe rimossa con successo."
+ChorDiscFail = "Connessione a Choregraphe non trovata."
+ChorDiscError = "Errore durante la rimozione della connessione a Choregraphe."
+
+Welcome1 = "Hai avviato il menù avanzato, spero che tu sappia cosa stai facendo!"
+Welcome2 = "Usa i sensori sulla mia testa per muoverti nel menù."
+
+OffsetChange = "Compenso: %s"
+OffsetSet = "Compenso impostato a %s!"
+OffsetChangeFail = "Non posso farlo!"
+
+UnknownError = "Errore sconosciuto."
+
+MainMenuChange = "%s selezionato!"
+
+Disconnect = "Disconnetti Choregraphe"
+WiFiOn = "Attiva WiFi"
+WiFiOff = "Disattiva WiFi"
+AutoLifeToggle = "Alterna la vita autonoma"
+ChangeOffsetFloor = "Cambia l'altezza dal pavimento"
+ChangeVolume = "Cambia il volume"
+Status = "Stato"
+FastReboot = "Riavvio rapido"
+Close = "Chiudi il menù"
+
+NothingSelected = "Non hai selezionato niente! Chiudo."
+
+WiFiError = "Errore, ALConnectionManager non è più disponibile"
+WifiActivated = "Tethering WiFi attivato."
+WifiAlreadyActive = "Il Tethering WiFi è già attivo."
+WifiDeactivated = "Tethering WiFi disattivato."
+WifiAlreadyInactive = "Il Tethering WiFi è già disattivato."
+
+VirtualRobotName = "Robot Virtuale"
+
+Name = "Il mio nome è %s \pau=300"
+Battery = "La mia batteria è al %d percento \pau=300"
+Autonomous = "Lo stato della mia vita autonoma è: %s."
+AutoSolitary = "Solitaria"
+AutoInteractive = "Interattiva"
+AutoDisabled = "Disattivata"
+AutoSafeGuard = "Protezione"
+WifiActive = "Il Tethering WiFi è attivo."
+UpperLetter = "%s maiuscola \pau=100"
+LowerLetter = "%s minuscola \pau=100"
+WifiSSID = "L' S S I D del Tethering WiFi è %s"
+WifiPassword = "La password del Tethering WiFi è %s"
+WifiInactive = "Il Tethering WiFi non è attivo."
+InternetOn = "Sono connesso a Internet."
+InternetOff = "Non sono connesso a Internet."
+
+Surrdender = "Mi arrendo."
+
+Reboot = "Mi sto riavviando!"
+
+# Do not change!
+Config_Version = 0.2
