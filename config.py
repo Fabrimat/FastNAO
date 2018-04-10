83e93902f6c5ba978f81eb2f4f899ddeb3b9c6d0
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2018-04-10 14:47:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
-language = "IT"
+# The language file you will be using
+# IT stands for IT-lang.py
+Language = "IT"
 
-NAO_IP = "127.0.0.1"
-NAO_PORT = 9559
+# The Nao ip, insert 127.0.0.1 if the app runs on the Nao
+Nao_IP = "127.0.0.1"
+# Naoqi port
+Nao_Port = 9559
 
-tetheringSSID = "Nao-WiFi"
-tetheringPassword = "nao12345"
-wifiCountry = "IT"
+Tethering_SSID = "Nao-WiFi"
+Tethering_Password = "nao12345"
+Wifi_Country = "IT"
 
-SaySSID = True
-SpellSSID = False
-SayPassowrd = False
-SpellPassword = False
+# Say SSID during Status
+Say_SSID = True
+# Spell SSID or not
+Spell_SSID = False
+# Say Password during Status
+Say_Passowrd = False
+# Spell Password or not
+Spell_Password = False
 
-logMode = "Both"
+# Whether to log to file or to naoqi
+# Options: file, naoqi, both
+# Case insensitive
+Log_Mode = "Both"
